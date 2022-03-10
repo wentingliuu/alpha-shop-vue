59f4942008b612a6f137f45666121c3526cc0988
wentingliu <cindy810317@gmail.com>
2022-03-10 15:09:31 +0800

    feat: project init & add 404 not found page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="not-found">
+    <h1>
+    404
+    </h1>
+    <h4>
+    PAGE NOT FOUND
+    </h4>
+    <a
+      href="#"
+      class="not-found_btn"
+    >
+      HOME PAGE
+    </a>
+  </div>
+</template>
