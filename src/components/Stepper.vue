29b541eb647277622566019bc9a47080190889f1
wentingliu <cindy810317@gmail.com>
2022-03-21 14:46:22 +0800

    feat: add stepper component

---
+++ src/components/Stepper.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="stepper" class="stepper">
+    <div class="step active">
+      <div class="step__circle"></div>
+      <h4 class="step__label">寄送地址</h4>
+    </div>
+    <span class="step__line"></span>
+    <div class="step">
+      <div class="step__circle"></div>
+      <h4 class="step__label">運送方式</h4>
+    </div>
+    <span class="step__line"></span>
+    <div class="step">
+      <div class="step__circle"></div>
+      <h4 class="step__label">付款資訊</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Stepper'
+}
+</script>
--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>This is the main page.</h1>
+  <div class="container">
+    <h1 class="main__title">結帳</h1>
+    
+    <section class="main__content">
+      <!-- left part-->
+      <div class="main__content__left">
+        <!-- stepper -->
+        <Stepper />
+      </div>
+    </section>
+  </div>
 </template>
+
+<script>
+import Stepper from '../components/Stepper'
+
+export default {
+  name: 'Checkout',
+  components: {
+    Stepper
+  }
+}
+</script>
